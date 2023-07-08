32bfa021bdf321ceabad1efb636c224f548adacd
takashi <tks.srtr89@gmail.com>
2023-07-08 19:06:35 +0900

    add files

--- samples/fold/main.hs
+++ samples/fold/main.hs
@@ -8,10 +8,16 @@
 
 -}
 
-
+{- リストの合計値を出す関数 -}
 sum' :: (Num a) => [a] -> a
 -- sum' xs = foldl (\acc x -> acc + x) 0 xs
 sum' = foldl (+) 0
 
+{- リストに値が含まれるかどうかを確認する関数 -}
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
 main = do
     print (sum' [3,5,2,1])
+    print (elem' 6 [3,5,2,1,9,4,6,7])
+    print (elem' 6 [3,5,2,1,9,4,7])
